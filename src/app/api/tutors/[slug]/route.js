147f3944532a556c3245cb11cbd8d5504aa1b0ce
saidur-way <saiduronway@gmail.com>
2023-08-30 23:51:56 +0600

    console hide

--- src/app/api/tutors/[slug]/route.js
+++ src/app/api/tutors/[slug]/route.js
@@ -6,11 +6,11 @@ import mongoose from 'mongoose'; // Import mongoose
 export const GET = async (request, { params }) => {
   try {
     await connect();
-    console.log(params)
+    // console.log(params)
 
     const { slug } = params; // Extract the tutor's id from the URL parameters
     if(slug){
-      console.log('slug show',slug)
+      // console.log('slug show',slug)
       // return {};
     }
     const tutorId = slug; // Replace with the actual _id value
@@ -21,7 +21,7 @@ export const GET = async (request, { params }) => {
     if (!tutor) {
       return new NextResponse("Tutor not found", { status: 404 });
     }
-    console.log("Tutor Data:", tutor);
+    // console.log("Tutor Data:", tutor);
     return new NextResponse(JSON.stringify(tutor), { status: 200 });
   } catch (error) {
     return new NextResponse("Database Error message!", { status: 500 });
